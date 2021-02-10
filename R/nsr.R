939ee70d0d535c9c015f425619294f3e561170d9
deepchocholate <andreas@systerdyster.se>
2021-02-10 19:16:41 +0100

    Updated

--- R/nsr.R
+++ R/nsr.R
@@ -9,24 +9,99 @@ checkState <- function (object) {
 }
 
 NSR = setClass("NSR",
-         representation(data="character", SUBJECTNAMES='vector', DESCRIPTIONS='list'),
          prototype = list(
            SUBJECTNAMES=names(SUBJECTNAMES),
            DESCRIPTIONS=SUBJECTNAMES
            ),
+         slots=list(
+           dataName="character",
+           subjectNames='list',
+           dataColumns='vector',
+           SUBJECTNAMES='vector',
+           DESCRIPTIONS='list'
+         ),
          validity=checkState)
-setMethod("initialize", "NSR", function (.Object, data) {
-  callNextMethod(.Object, data=deparse(substitute(data)))
+# Constructor
+setMethod("initialize", "NSR", function (.Object, data, subjects=F) {
+  dn = deparse(substitute(data))
+  #print(colnames(eval(parse(text=dn), envir = globalenv())))
+  #.Object = setSubjects(.Object, colnames(data))
+  .Object = setColumns(.Object, colnames(data))
+  if (!is.list(subjects)) {
+    subjects = as.list(.Object@SUBJECTNAMES)
+    names(subjects) = .Object@SUBJECTNAMES
+  }
+  .Object = setSubjectNames(.Object, subjects)
+  callNextMethod(.Object, dataName=dn)
 })
-setGeneric("GPA", function (object, data, subjects=F) standardGeneric("GPA"))
-setMethod("GPA", signature("NSR"), function (object, data, subjects=F) {
-  if (!is.vector(subjects)) subjects <- object@SUBJECTNAMES
-  subjects <- base::intersect(object@SUBJECTNAMES, subjects)
-  rowMeans(data[,subjects])
-})
-setGeneric("setSubjects", function (subjects, object) standardGeneric("setSubjects"))
-setMethod("setSubjects", signature(subjects="list", object="NSR"),
-          function (subjects, object) {
-            object@subjects <- subjects
+# Match grade columns (for internal use only)
+# Returns a vector
+setGeneric("matchGradeNames", function (object) standardGeneric("matchGradeNames"))
+setMethod("matchGradeNames", signature("NSR"),
+          function (object) {
+            base::intersect(object@SUBJECTNAMES, object@subjectNames)
+          })
+# Get available grade columns names in data
+setGeneric("getGradeColumns", function (object) standardGeneric("getGradeColumns"))
+setMethod("getGradeColumns", signature("NSR"),
+          function (object) {
+            base::intersect(object@dataColumns, as.vector(unlist(object@subjectNames)))
+          })
+# Set data columns
+setGeneric("setColumns", function (object, cols) standardGeneric("setColumns"))
+setMethod("setColumns", signature("NSR", "vector"),
+          function (object, cols) {
+            object@dataColumns <- cols
             object
           })
+# Set subjects
+setGeneric("setSubjectNames", function (object, subjectNames) standardGeneric("setSubjectNames"))
+setMethod("setSubjectNames", signature("NSR", "list"),
+          function (object, subjectNames) {
+            # Get subjects not in subjectNames
+            keys = names(subjectNames)
+            # Unassigned
+            unass = base::setdiff(object@SUBJECTNAMES, keys)
+            ass = base::intersect(object@SUBJECTNAMES, keys)
+            newKeys = c(unass, ass)
+            newSubjects = c(unass, as.vector(unlist(subjectNames)))
+            temp <- as.list(newSubjects)
+            names(temp) <- newKeys
+            object@subjectNames <- temp
+            object
+          })
+
+#' Calculate grade-point average (GPA)
+#'
+#' @param object An object of class NSR.
+#' @param subjects A list of subjects to include in the GPA
+#' @rdname GPA
+setGeneric("GPA", function (object, subjects=F) standardGeneric("GPA"))
+#' @rdname GPA
+setMethod("GPA", signature("NSR"),
+          function (object, subjects=F) {
+            if (!is.vector(subjects)) subjects <- object@subjectNames
+            subjects <- getGradeColumns(object)
+            dta = eval(parse(text=object@dataName), envir=globalenv())[,subjects]
+            rowMeans(dta)
+          })
+
+# To convert object to a data frame
+setMethod("as.vector", signature("NSR"),
+          function (x, mode) {
+            as.vector(GPA(x), mode)
+          })
+# To assign something of the data to a data.frame
+setMethod("rep", signature("NSR"),
+          function (x, length.out) {
+            GPA(x)
+          })
+"
+data is Math, HI
+a <- NSR(data) or
+a <- NSR(data, list(MA='Math'))
+a@subjectNames returns list(MA='Math', HI='HI')
+devtools::document()
+a <- NSR(students, subjects=list(EN='Eng'))
+GPA(a)
+"

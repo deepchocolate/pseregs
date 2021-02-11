1741447d45e5c866ca1c6f3513ce512e50f3c732
deepchocholate <andreas@systerdyster.se>
2021-02-11 14:26:25 +0100

    Update

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -2,7 +2,7 @@
 DESCRIPTIONS <- list(
   PROVAR="Graduation year",
   KOMMUN="Municipality/parish",
-  HMAN="",
+  HMAN="Huvudman",
   MA_A_PROV="",
   MA_B1_PROV="",
   MA_B2_PROV="",
@@ -55,4 +55,5 @@ TESTS = c("SV_A_BETYG","SV_B_BETYG","SV_C_BETYG","SV_PROVBET","EN_A_BETYG","EN_B
           "MA_E_POT","MA_C_POT","MA_A_POT","MA_E_POM","MA_C_POM","MA_A_POM","MA_A_PROV","MA_B1_PROV","MA_B2_PROV",
           "MA_B_PROV","MA_C_PROV","MA_D_PROV","MA_G_PROV","MA_VG_PROV")
 
-usethis::use_data(GRADES,TESTS,DESCRIPTIONS, internal=T,overwrite=T)
+usethis::use_data(GRADES,TESTS,DESCRIPTIONS, overwrite=T,internal=T)
+#

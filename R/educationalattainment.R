b58ab9036d8066f64a92e25d1031ff27ba3ec9bf
deepchocolate <andreas@systerdyster.se>
2021-11-16 14:59:03 +0100

    Fixed error

--- R/educationalattainment.R
+++ R/educationalattainment.R
@@ -2,14 +2,17 @@
 #' @description Calculate educational attainment level according to Statistics Swedens classification of education.
 #' @details This function takes educational attainment codes according to Statistics Swedens classification
 #' (mostly named SUN2000NIVA in datasets) and recodes
-#' them into three levels where compulsory is equivalient of at most 9 years of education, secondary 9 to 12 years, and
+#' them into three levels where compulsory is equivalient of at most 9 years of education, secondary 9 to 12 years (one exception), and
 #' tertiary greater than 12 years. Warnings are generated if data contains codes that are unknown according to the offical
 #' documentation from statistics Sweden.
+#'
+#' Some post secondary educations are included in secondary and these are shoften shorter than 2 years and occur outside university or
+#' job oriented educations shorter than 2 years at a university. See codes 410-417.
 #' @examples # 2 with compulsory education, 1 with secondary, 1 with tertiary, 2 missing
-#' sunCodes <- c('001','200','312', '415','999', NA)
+#' sunCodes <- c('001','200','312', '415', '525','999', NA)
 #' newCodes <- educationalAttainment(sunCodes, list(compulsory=1, secondary=2, tertiary=3))
 #' codes
-#' # [1] "1", "1", "2", "3", NA, NA
+#' # [1] "1", "1", "2", "2", "3", NA, NA
 #' @param object A vector of SUN2000NIVA codes
 #' @param attainments A named list of educational attainments to code, eg list(compulsory='mycode1', secondary='mycode2', tertiary='mycode3')
 #' @param missing The character code for a missing observation, defaults to '999' and are treated as other missing values in data.
@@ -34,8 +37,8 @@ EA <- function (object, attainments, codeLevel, missingCode='999') {
   outputs <- object
   outputs[!is.na(outputs) & codes == '9'] <- NA
   outputs[!is.na(outputs) & codes <= '2'] <- attainments[['compulsory']]
-  outputs[!is.na(outputs) & codes == '3'] <- attainments[['secondary']]
-  outputs[!is.na(outputs) & codes >= '4'] <- attainments[['tertiary']]
+  outputs[!is.na(outputs) & codes >= '3' & codes <= '4'] <- attainments[['secondary']]
+  outputs[!is.na(outputs) & codes >= '5'] <- attainments[['tertiary']]
   nComp <- sum(outputs == attainments[['compulsory']], na.rm=T)
   nSec <- sum(outputs == attainments[['secondary']], na.rm=T)
   nTer <- sum(outputs == attainments[['tertiary']], na.rm=T)
